8180d7467c8df2d5ed1ba4a8c1013efe4594a94f
Patrick Herrmann <patrickwherrmann@gmail.com>
2012-10-16 09:09:43 -0700

    Initial commit. Script generates only ground state patterns.

---
+++ siteswap.py
@@ -0,0 +1,27 @@
+props = 4
+maxheight = 6
+maxlength = 5
+exclude = [0]
+
+tossHeights = filter(lambda x: x not in exclude, range(maxheight + 1))
+groundState = map(lambda x: x < props, range(maxheight + 1))
+
+def buildSiteswap(pattern, wheel, length):
+    availTosses = filter(lambda x: not wheel[x], tossHeights)
+    if not wheel[0]: availTosses = availTosses[:1]
+
+    if len(pattern) > 0 and wheel == groundState:
+        print "".join(str(x) for x in pattern)
+    elif length > 0:
+        for toss in availTosses:
+            nextPattern = list(pattern)
+            nextPattern.append(toss)
+            buildSiteswap(nextPattern, performToss(list(wheel), toss), length - 1)
+
+def performToss(wheel, toss):
+    wheel[toss] = True
+    del wheel[0]
+    wheel.append(False)
+    return wheel
+
+buildSiteswap([], groundState, maxlength)
